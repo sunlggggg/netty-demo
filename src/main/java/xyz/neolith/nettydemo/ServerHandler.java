ea7b8682ee251d4b8d259ba8b1023b1524016286
sunlggggg <sunlggggg@gmail.com>
2018-07-26 13:34:46 +0800

    feat:first serve

---
+++ src/main/java/xyz/neolith/nettydemo/ServerHandler.java
@@ -0,0 +1,33 @@
+package xyz.neolith.nettydemo;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+
+/**
+ * @author sunligang
+ * @date 2018/07/26
+ */
+public class ServerHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ByteBuf in = (ByteBuf) msg;
+        try {
+            while (in.isReadable()) {
+                System.out.print((char) in.readByte());
+                System.out.flush();
+            }
+        } finally {
+            ReferenceCountUtil.release(msg);
+        }
+    }
+
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
+        // Close the connection when an exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
